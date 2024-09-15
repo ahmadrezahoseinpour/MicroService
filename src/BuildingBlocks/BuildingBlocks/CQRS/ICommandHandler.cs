872fea91e57bc7b76f5840148a5992d2fcee3468
hosseinpour.a <hoseinpour.a@daneshrefah.ir>
2024-09-15 19:13:11 +0430

    [2024/09/15] Add Biulding Blocks

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in TCommon> 
+    : ICommandHandler<TCommon, Unit>
+    where TCommon : ICommand<Unit>
+{
+}
+
+public interface ICommandHandler<in TCommand, TResponse> 
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
