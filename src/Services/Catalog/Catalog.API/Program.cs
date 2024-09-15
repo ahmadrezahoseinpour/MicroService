ee9031d3a61bb24e1c0c429d32e975c209d8e9be
hosseinpour.a <hoseinpour.a@daneshrefah.ir>
2024-09-15 19:09:42 +0430

    [2024/09/15] Update Add Docker

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,23 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.Run();
+app.MapCarter();
+app.Run(); 
